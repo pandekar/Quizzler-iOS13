dc7579d54eed38fa58f67a96caa042d9fbe07af6
pandekar <pandeadhistanaya@gmail.com>
2022-12-25 19:57:01 +0800

    implement MVC pattern

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain: QuizBrain = QuizBrain()
+    let answerRightColor: UIColor = UIColor.green
+    let answerWrongColor: UIColor = UIColor.red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let buttonTitle: String = sender.currentTitle!
+        let submittedAnswer: String = buttonTitle.lowercased()
+        
+        if quizBrain.checkAnswer(submittedAnswer) {
+            updateUI()
+            animateUIButton(buttonPressed: buttonTitle, color: answerRightColor)
+        } else {
+            animateUIButton(buttonPressed: buttonTitle, color: answerWrongColor)
+        }
+        
+    }
+    
+    func updateUI() {
+        
+        if quizBrain.getCurrentQuestionNumber() == 0 {
+            progressBar.progress = 1/Float(quizBrain.getTotalQuestionNumber())
+        } else {
+            progressBar.progress += 1/Float(quizBrain.getTotalQuestionNumber())
+        }
+        
+        questionLabel.text = quizBrain.getQuizText()
+    }
+    
+    // trigger button animation
+    func animateUIButton(buttonPressed: String, color: UIColor) {
+        if (buttonPressed == "True") {
+            buttonAnimation(selectedButton: self.trueButton, color: color)
+        } else {
+            buttonAnimation(selectedButton: self.falseButton, color: color)
+        }
+    }
+    
+    // UIButton Animation
+    func buttonAnimation(selectedButton: UIButton, color: UIColor) {
+        UIView.animate(withDuration: 0.2) {
+            selectedButton.backgroundColor = color
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            UIView.animate(withDuration: 0.2) {
+                selectedButton.backgroundColor = nil
+            }
+        }
+    }
+}
+
---
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,61 @@
+//
+//  QuizBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by Pande Adhistanaya on 25/12/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct QuizBrain {
+    
+    // question data
+    let quiz: [Question] = [
+        Question(q: "A slug's blood is green.", a: true),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: true),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: true),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: true),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: false),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: true),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: false),
+        Question(q: "Google was originally called 'Backrub'.", a: true),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: true),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: false),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: false),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: false)
+    ]
+    
+    // question number
+    var questionNumber: Int = 0
+    
+    // get total question number
+    func getTotalQuestionNumber() -> Int {
+        return quiz.count
+    }
+    
+    // get current question number
+    func getCurrentQuestionNumber() -> Int {
+        return questionNumber
+    }
+    
+    // get quiz text
+    func getQuizText() -> String {
+        return quiz[self.questionNumber].text
+    }
+    
+    // check answer
+    mutating func checkAnswer(_ userAnswer: String) -> Bool {
+        let userAnswer: Bool = Bool(userAnswer)!
+        
+        if(userAnswer == quiz[questionNumber].answer) && (questionNumber < quiz.count - 1) {
+            self.questionNumber += 1
+            return true
+        } else if(userAnswer == quiz[questionNumber].answer) && (questionNumber == quiz.count - 1) {
+            self.questionNumber = 0
+            return true
+        } else {
+            return false
+        }
+    }
+}
